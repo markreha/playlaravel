3dcc3bffec2468a710025b07e91db71477e58088
Mark Reha <markreha@mac.com>
2020-03-29 09:59:21 -0700

    Update MyLogger2.php

--- app/Services/Utility/MyLogger2.php
+++ app/Services/Utility/MyLogger2.php
@@ -16,7 +16,7 @@ class MyLogger2 implements ILogger
         {
             self::$logger = new Logger('MyApp');
             //$stream = new StreamHandler('storage/logs/myapp.log', Logger::DEBUG);
-            $stream = new StreamHandler('b79d48aa-0105-4d8b-a5ee-4b9069eed8d3/tag/cst323_logfile_heroku_upload_php', Logger::INFO);
+            $stream = new StreamHandler('b79d48aa-0105-4d8b-a5ee-4b9069eed8d3/tag/cst323_logfile_heroku_upload_php', Logger::DEBUG);
             $stream->setFormatter(new LineFormatter("%datetime% : %level_name% : %message% %context%\n", "g:iA n/j/Y"));
             self::$logger->pushHandler($stream);
         }
